a9a80a9c4ad1631039d47ff60bb27a435cd6136d
Calvin Oker <calvinoker8@gmail.com>
2023-07-30 13:19:39 +0300

    first commit

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
 
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,19 +11,26 @@ const port = process.env.PORT || 5000;
 // use body parser to get data from POST requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
 // Use API routes from the api folder
 const apis = require("./api");
 app.use("/api", apis);
 
+// If in production, then use static frontend build files.
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
-// Connect to Mongo DB Atlas
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+}
+
+// Connect to Mongo
 mongoose.connect(process.env.DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB Connected to Weather App..'))
-.catch(err => console.log(err));
+}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
