21f32ecf8e733f652454f781fac83f0fb28a45d6
Calvin Oker <calvinoker8@gmail.com>
2023-08-01 00:29:47 +0300

    modified

---
+++ index.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+//const __dirname = path.resolve();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// use body parser to get data from POST requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// Use API routes from the api folder
+const apis = require("./api");
+app.use("/api", apis);
+
+
+//static files serving the frontend
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (req,res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+        res.status(500).send(err);
+    })
+
+});
+
+
+// Connect to Mongo DB Atlas
+mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected to Weather App..'))
+.catch(err => console.log(err));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
